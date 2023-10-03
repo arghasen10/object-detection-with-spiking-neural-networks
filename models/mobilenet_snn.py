a698517e3916f5cff4e1e7fa8fde3e5b175aaaf7
arghasen10 <arghasen10@gmail.com>
2023-10-03 05:49:53 +0530

    working mobilenet

--- models/mobilenet_snn.py
+++ models/mobilenet_snn.py
@@ -4,20 +4,20 @@ import snntorch as snn
 from snntorch import surrogate
 
 class DepthwiseSeparableConv(nn.Module):
-    def __init__(self, in_channels, out_channels, stride, spike_grad = surrogate.atan()):
+    def __init__(self, in_channels, out_channels, stride, beta_shape, spike_grad = surrogate.atan()):
         super(DepthwiseSeparableConv, self).__init__()
         self.spike_grad = spike_grad
         self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=stride, padding=1, groups=in_channels, bias=False)
-        self.beta1 = torch.rand(in_channels)
-        self.thr1 = torch.rand(in_channels)
+        self.beta1 = torch.rand(beta_shape)
+        self.thr1 = torch.rand(beta_shape)
         self.lif1 = snn.Leaky(beta=self.beta1, threshold=self.thr1, learn_beta=True, learn_threshold=True, spike_grad=self.spike_grad)
         self.bn1 = nn.BatchNorm2d(in_channels)
-        self.beta2 = torch.rand(in_channels)
-        self.thr2 = torch.rand(in_channels)
+        self.beta2 = torch.rand(beta_shape)
+        self.thr2 = torch.rand(beta_shape)
         self.lif2 = snn.Leaky(beta=self.beta2, threshold=self.thr2, learn_beta=True, learn_threshold=True, spike_grad=self.spike_grad)
         self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
-        self.beta3 = torch.rand(out_channels)
-        self.thr3 = torch.rand(out_channels)
+        self.beta3 = torch.rand(beta_shape)
+        self.thr3 = torch.rand(beta_shape)
         self.lif3 = snn.Leaky(beta=self.beta3, threshold=self.thr3, learn_beta=True, learn_threshold=True, spike_grad=self.spike_grad)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.beta4 = torch.rand(out_channels)
@@ -42,7 +42,7 @@ class MobileNetV1(nn.Module):
     def __init__(self, num_classes=2, spike_grad = surrogate.atan()):
         super(MobileNetV1, self).__init__()
         self.spike_grad = spike_grad
-        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=2, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(4, 32, kernel_size=3, stride=2, padding=1, bias=False)
         self.beta1 = torch.rand(32)
         self.thr1 = torch.rand(32)
         self.lif1 = snn.Leaky(beta=self.beta1, threshold=self.thr1, learn_beta=True, learn_threshold=True, spike_grad=self.spike_grad)
@@ -50,20 +50,20 @@ class MobileNetV1(nn.Module):
         self.beta2 = torch.rand(32)
         self.thr2 = torch.rand(32)
         self.lif2 = snn.Leaky(beta=self.beta2, threshold=self.thr2, learn_beta=True, learn_threshold=True, spike_grad=self.spike_grad)
-        self.dsconv1 = DepthwiseSeparableConv(32, 64, stride=1)
-        self.dsconv2 = DepthwiseSeparableConv(64, 128, stride=2)
-        self.dsconv3 = DepthwiseSeparableConv(128, 128, stride=1)
-        self.dsconv4 = DepthwiseSeparableConv(128, 256, stride=2)
-        self.dsconv5 = DepthwiseSeparableConv(256, 256, stride=1)
-        self.dsconv6 = DepthwiseSeparableConv(256, 512, stride=2)
-        self.dsconv7 = DepthwiseSeparableConv(512, 512, stride=1)
-        self.dsconv8 = DepthwiseSeparableConv(512, 512, stride=1)
-        self.dsconv9 = DepthwiseSeparableConv(512, 512, stride=1)
-        self.dsconv10 = DepthwiseSeparableConv(512, 1024, stride=2)
-        self.dsconv11 = DepthwiseSeparableConv(1024, 1024, stride=1)
+        self.dsconv1 = DepthwiseSeparableConv(32, 64, stride=1, beta_shape=32)
+        self.dsconv2 = DepthwiseSeparableConv(64, 128, stride=2, beta_shape=16)
+        self.dsconv3 = DepthwiseSeparableConv(128, 128, stride=1, beta_shape=16)
+        self.dsconv4 = DepthwiseSeparableConv(128, 256, stride=2, beta_shape=8)
+        self.dsconv5 = DepthwiseSeparableConv(256, 256, stride=1, beta_shape=8)
+        self.dsconv6 = DepthwiseSeparableConv(256, 512, stride=2, beta_shape=4)
+        self.dsconv7 = DepthwiseSeparableConv(512, 512, stride=1, beta_shape=4)
+        self.dsconv8 = DepthwiseSeparableConv(512, 512, stride=1, beta_shape=4)
+        self.dsconv9 = DepthwiseSeparableConv(512, 512, stride=1, beta_shape=4)
+        self.dsconv10 = DepthwiseSeparableConv(512, 1024, stride=2, beta_shape=2)
+        self.dsconv11 = DepthwiseSeparableConv(1024, 1024, stride=1, beta_shape=2)
         self.avgpool = nn.AdaptiveAvgPool2d(1)
-        self.beta3 = torch.rand(32)
-        self.thr3 = torch.rand(32)
+        self.beta3 = torch.rand(1)
+        self.thr3 = torch.rand(1)
         self.lif3 = snn.Leaky(beta=self.beta3, threshold=self.thr3, learn_beta=True, learn_threshold=True, spike_grad=self.spike_grad)
         self.fc = nn.Linear(1024, num_classes)
         self.beta4 = torch.rand(num_classes)
@@ -80,21 +80,22 @@ class MobileNetV1(nn.Module):
         spk_rec = []
         self.init_leaky()
         for step in range(x1.size(0)):  # data.size(0) = number of time steps
-            out1, self.mem1 = self.lif1(self.conv1(x1[step]), self.mem1)
-            out2, self.mem2 = self.lif2(self.bn1(out1), self.mem2)        
-            out3 = self.dsconv1(out2)
-            out4 = self.dsconv2(out3)
-            out5 = self.dsconv3(out4)
-            out6 = self.dsconv4(out5)
-            out7 = self.dsconv5(out6)
-            out8 = self.dsconv6(out7)
-            out9 = self.dsconv7(out8)
-            out10 = self.dsconv8(out9)
-            out11 = self.dsconv9(out10)
-            out12 = self.dsconv10(out11)
-            out13 = self.dsconv11(out12)
-            out14, self.mem3 = self.lif3(self.avgpool(out13), self.mem3)
-            out15 = out14.view(-1, 1024)
-            out, self.mem4 = self.lif4(self.fc(out15), self.mem4)
+            # Input shape [64, 4, 64, 64]
+            out1, self.mem1 = self.lif1(self.conv1(x1[step]), self.mem1)            # [64, 32, 32, 32]
+            out2, self.mem2 = self.lif2(self.bn1(out1), self.mem2)                  # [64, 32, 32, 32]   
+            out3 = self.dsconv1(out2)                                               # [64, 64, 32, 32]
+            out4 = self.dsconv2(out3)                                               # [64, 128, 16, 16]
+            out5 = self.dsconv3(out4)                                               # [64, 128, 16, 16]
+            out6 = self.dsconv4(out5)                                               # [64, 256, 8, 8]
+            out7 = self.dsconv5(out6)                                               # [64, 256, 8, 8]
+            out8 = self.dsconv6(out7)                                               # [64, 512, 4, 4]
+            out9 = self.dsconv7(out8)                                               # [64, 512, 4, 4]
+            out10 = self.dsconv8(out9)                                              # [64, 512, 4, 4]
+            out11 = self.dsconv9(out10)                                             # [64, 512, 4, 4]
+            out12 = self.dsconv10(out11)                                            # [64, 1024, 2, 2]
+            out13 = self.dsconv11(out12)                                            # [64, 1024, 2, 2]
+            out14, self.mem3 = self.lif3(self.avgpool(out13), self.mem3)            # [64, 1024, 1, 1]
+            out15 = out14.view(-1, 1024)                                            # [64, 1024]
+            out, self.mem4 = self.lif4(self.fc(out15), self.mem4)                   # [64, 2]
             spk_rec.append(out)
         return torch.stack(spk_rec)
